f181277a926698e83838371b410d28255c5c9289
takker99 <37929109+takker99@users.noreply.github.com>
2023-01-15 17:03:32 +0900

    :recycle: Move dependencies of D3.js to deps/d3.ts

---
+++ deps/d3.ts
@@ -0,0 +1,10 @@
+export * from "https://cdn.skypack.dev/d3-selection@v3.0.0?dts";
+export * from "https://cdn.skypack.dev/d3-zoom@v3.0.0?dts";
+export * from "https://cdn.skypack.dev/d3-drag@v3.0.0?dts";
+export type { D3ZoomEvent } from "https://cdn.skypack.dev/d3-zoom@v3.0.0?dts";
+export type { D3DragEvent } from "https://cdn.skypack.dev/d3-drag@v3.0.0?dts";
+export * from "https://cdn.skypack.dev/d3-force@v3.0.0?dts";
+export type {
+  SimulationLinkDatum,
+  SimulationNodeDatum,
+} from "https://cdn.skypack.dev/d3-force@v3.0.0?dts";
